906ce50956cb18ff02b63e09b3eb856c0aa66c67
Douglas Rodrigues <douglas.x.963@gmail.com>
2023-09-27 13:42:03 -0300

    finalizado

---
+++ src/com/contador/terminal/parametrosInvalidosException.java
@@ -0,0 +1,9 @@
+package com.contador.terminal;
+
+public class parametrosInvalidosException extends Exception {
+
+	public parametrosInvalidosException() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
